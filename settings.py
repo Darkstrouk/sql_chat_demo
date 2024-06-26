2e120ec2cf531aecc819d4702da9b4037ad5883b
Darkstrouk <Avborodin@edu.hse.ru>
2024-06-26 15:18:19 +0300

    added settings_developer. also added vars in both settings - dev and server

--- settings.py
+++ settings.py
@@ -1,5 +1,16 @@
 """Файл для глобальных переменных проекта"""
 
+import json
+import os
+import pathlib
 
-with open('/Users/darkstrouk/Documents/Сбер/ЦКР/test_sql_chat/sql_chat/database/utils/ddl.txt', 'r', encoding='utf-8') as file:
+
+SQLITE_DB_PATH = 'root/sql_chat_demo/database/utils/demo.db'
+PROMPT_PATH = 'root/sql_chat_demo/database/utils/ddl.txt'
+
+with open(PROMPT_PATH, 'r', encoding='utf-8') as file:
     PROMPT = file.read()
+
+
+if os.path.exists('settings_developer.py'):
+    from settings_developer import * #noqa
